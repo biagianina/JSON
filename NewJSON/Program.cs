7a3a7cff3fd9f04100f5b3a4263c74e9f5ab1164
bianca gianina <biagianina02@gmail.com>
2019-07-31 23:56:15 +0300

    up

---
+++ NewJSON/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NewJSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
