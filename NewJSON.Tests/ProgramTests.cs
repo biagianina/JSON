947285a128380e9d5b7a4f1f0996ffe3263a93cd
bianca gianina <biagianina02@gmail.com>
2019-08-07 21:38:32 +0300

    Returns "Invalid" for an empty string

--- NewJSON.Tests/ProgramTests.cs
+++ NewJSON.Tests/ProgramTests.cs
@@ -6,75 +6,9 @@ namespace NewJSON.Tests
     public class ProgramTests
     {
         [Fact]
-        public void StringStartsAndEndsWithQuotations()
+        public void StringIsNullShouldReturnInvalid()
         {
-            Assert.Equal("Valid", Program.JSONValidator("\"Test\""));
-        }
-
-        [Fact]
-        public void StringOnlyStartsWithQuotations()
-        {
-            Assert.Equal("Invalid", Program.JSONValidator("\"Test"));
-        }
-
-        [Fact]
-        public void StringOnlyEndsWithQuotations()
-        {
-            Assert.Equal("Invalid", Program.JSONValidator("Test\""));
-        }
-
-        [Fact]
-        public void StringContainsQuotations()
-        {
-            Assert.Equal("Invalid", Program.JSONValidator("\"Te\"st\""));
-        }
-
-        [Fact]
-        public void StringContainsSlash()
-        {
-            Assert.Equal("Invalid", Program.JSONValidator("\"Te/st\""));
-        }
-
-        [Fact]
-        public void StringContainsBackSlashAlone()
-        {
-            Assert.Equal("Invalid", Program.JSONValidator("\"Te\\st\""));
-        }
-
-        [Fact]
-        public void StringContainsBackSlashCharacters()
-        {
-            Assert.Equal("Valid", Program.JSONValidator("\"Te\\nst\""));
-        }
-
-        [Fact]
-        public void StringContainsUnicodeCharacters()
-        {
-            Assert.Equal("Valid", Program.JSONValidator("\"Te\\\\u0097st\""));
-        }
-
-        [Fact]
-        public void StringContainsSpaces()
-        {
-            Assert.Equal("Valid", Program.JSONValidator("\"Te st\""));
-        }
-
-        [Fact]
-        public void StringContainsCorrectSlash()
-        {
-            Assert.Equal("Valid", Program.JSONValidator("\"Te\\/st\""));
-        }
-
-        [Fact]
-        public void StringContainsCorrectQuotations()
-        {
-            Assert.Equal("Valid", Program.JSONValidator("\"Te\\\"st\""));
-        }
-
-        [Fact]
-        public void StringIsComplex()
-        {
-            Assert.Equal("Valid", Program.JSONValidator("\"Test\\\\u0097\\nAnother line\""));
+            Assert.Equal("Invalid", Program.JSONValidator(""));
         }
     }
 }
--- NewJSON/Program.cs
+++ NewJSON/Program.cs
@@ -11,91 +11,14 @@ namespace NewJSON
             Console.Read();
         }
 
-#pragma warning disable S1541 // Methods and properties should not be too complex
         public static string JSONValidator(string input)
-#pragma warning restore S1541 // Methods and properties should not be too complex
         {
-            const string invalid = "Invalid";
             if (string.IsNullOrEmpty(input))
             {
-                return invalid;
-            }
-
-            if (!input.StartsWith('\"') || !input.EndsWith('\"'))
-            {
-                return invalid;
-            }
-
-            for (int i = 1; i < input.Length - 1; i++)
-            {
-                if (input[i - 1] != '\\' && input[i] == '/' || input[i - 1] != '\\' && input[i] == '"')
-                {
-                    return invalid;
-                }
-
-                const int charactersLimit = 32;
-                if (input[i] < charactersLimit)
-                {
-                    return invalid;
-                }
-
-                if (input[i] == '\\' && !CheckBackslash(input.Substring(input.IndexOf('\\') + 1)))
-                {
-                     return invalid;
-                }
+                return "Invalid";
             }
 
             return "Valid";
-        }
-
-        private static bool CheckBackslash(string substring)
-            {
-            if (CheckBackslashNextCharacter(substring[0]))
-            {
-                return true;
-            }
-
-            if (substring[1] != 'u')
-            {
-                return false;
-            }
-
-            const int unicodeLength = 5;
-            const int start = 2;
-            return CheckUnicode(substring.Substring(start, unicodeLength));
-        }
-
-        private static bool CheckBackslashNextCharacter(char nextCharacter)
-        {
-            char[] validCharacters = { 'b', 'f', 'n', 'r', 't', '"', '/' };
-            for (int i = 0; i < validCharacters.Length; i++)
-            {
-                if (nextCharacter == validCharacters[i])
-                {
-                    return true;
-                }
-            }
-
-            return false;
-        }
-
-        private static bool CheckUnicode(string unicode)
-        {
-            const int A = 65;
-            const int F = 70;
-            const int a = 97;
-            const int f = 102;
-            foreach (char c in unicode)
-            {
-#pragma warning disable S1067 // Expressions should not be too complex
-                if ((c >= A && c <= F) || (c >= a && c <= f) || char.IsDigit(c))
-#pragma warning restore S1067 // Expressions should not be too complex
-                {
-                    return true;
-                }
-            }
-
-            return false;
         }
     }
 }
